44cddd1b1bfe979385e39742f17c8c6b968769f0
Fernando Guillen <fguillen.mail@gmail.com>
2009-12-21 00:33:44 -0500

    migration to haml

--- server.rb
+++ server.rb
@@ -1,14 +1,14 @@
 require 'rubygems'
 require 'sinatra'
-require 'erb'
 require "#{File.dirname(__FILE__)}/lib/scrappers/conjugation_org_scrapper.rb"
+require 'haml'
 
 get '/' do
-  erb(:search)
+  haml :search
 end
 
 get '/:q' do
   @results = Scrappers::ConjugationOrgScrapper.verb( params[:q] )
-  halt(404, erb(:not_results))  if @results.empty?
-  erb(:show)
+  halt(404, haml(:not_results))  if @results.empty?
+  haml(:show)
 end
